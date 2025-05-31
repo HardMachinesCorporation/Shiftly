3c2db2509d8d6b3fe92897be35b3766647eac455
HardMachinesCorporation <thejordach@gmail.com>
2025-05-31 15:31:59 -0700

    chore: config theme (#2)
    * chore: config theme
    
    * updating lint.yml

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    <!-- Bouton principal -->
+    <button class="bg-primary text-white hover:bg-secondary">
+      Add Task
+    </button>
+
+    <!-- Carte -->
+    <div class="bg-soft text-accent p-4 rounded-xl shadow-md">
+      Task transferred from Alice
+    </div>
+
+    <!-- Texte coloré -->
+    <p class="text-shiftly-violet">
+      You have 2 transfer requests
+    </p>
+  </div>
 </template>
---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './components/**/*.{vue,ts}',
+    './pages/**/*.{vue,ts}',
+    './layouts/**/*.{vue,ts}',
+    './app.vue',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F11A7B',
+        secondary: '#982176',
+        accent: '#3E001F',
+        soft: '#FFE5AD',
+        // Optionnel : variantes nommées
+        shiftly: {
+          pink: '#F11A7B',
+          violet: '#982176',
+          wine: '#3E001F',
+          sand: '#FFE5AD',
+
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
